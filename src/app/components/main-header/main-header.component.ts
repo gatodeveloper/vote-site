a16e88d8296429563ab6a66a7cdbedf043380eca
Diego Santiesteban <gatodeveloper@gmail.com>
2019-11-27 13:57:17 -0500

    Added mobile menu and responsive 98%

--- src/app/components/main-header/main-header.component.ts
+++ src/app/components/main-header/main-header.component.ts
@@ -7,6 +7,8 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MainHeaderComponent implements OnInit {
 
+  public mobileMenuOpened = false;
+
   constructor() { }
 
   ngOnInit() {
